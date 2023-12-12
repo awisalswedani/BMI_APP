9a34ddf508059149e9a492479d1f4caa8b629602
Mohammad Awis Alswedani <owis1310@gmail.com>
2023-12-13 00:16:56 +0300

    First Commit

---
+++ lib/home.dart
@@ -0,0 +1,208 @@
+import 'dart:math';
+
+import 'package:bmi/result.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isMale = true;
+  double heightVal = 170;
+
+  int weight = 55;
+  int age = 18;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Body Mass Index"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(7),
+                child: Row(
+                  children: [
+                    m1Expanded(context, "male"),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    m1Expanded(context, "female"),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: Container(
+                  decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.blueGrey,
+          ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Height", style: Theme.of(context).textTheme.headline2,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline :TextBaseline.alphabetic,
+                        children: [
+                          Text(heightVal.toStringAsFixed(1), style: Theme.of(context).textTheme.headline1,),
+                          Text("CM", style: Theme.of(context).textTheme.bodyText1,),
+                        ],
+                      ),
+                      Slider(
+                        min: 90,
+                        max: 220,
+                        value: heightVal, 
+                        onChanged: (newValue) => setState(() => heightVal = newValue)
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    m2Expanded(context, "weight"),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    m2Expanded(context, "age"),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.teal,
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 14,
+              child: TextButton(
+                onPressed: () {
+                  var result = weight / pow(heightVal / 100, 2);
+                  print(result);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Result(result: result, isMale: isMale, age: age);
+                      },
+                    ),
+                  );
+                },
+                child: const Text(
+                  "Calculate",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded m1Expanded(BuildContext context, String type) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => setState(() => isMale = (type == "male") ? true : false),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: (isMale && type == "male") || (!isMale && type == "female")
+                ? Colors.teal
+                : Colors.blueGrey,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(type == "male" ? Icons.male : Icons.female),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                type == "male" ? "Male" : "female",
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded m2Expanded(BuildContext context, String type) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.blueGrey,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              type == "age" ? "age" : "weight",
+              style: Theme.of(context).textTheme.headline2,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              type == "age" ? "$age" : "$weight",
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  heroTag: type == "age" ? "age--" : "weight--",
+                  onPressed: () {
+                    setState(() {
+                      type == "age" ? age-- : weight--;
+                    });
+                  },
+                  child: Icon(
+                    Icons.remove,
+                  ),
+                  mini: true,
+                ),
+                const SizedBox(width: 8),
+                FloatingActionButton(
+                  heroTag: type == "age" ? "age++" : "weight++",
+                  onPressed: () {
+                    setState(() {
+                      type == "age" ? age++ : weight++;
+                    });
+                  },
+                  child: const Icon(
+                    Icons.add,
+                  ),
+                  mini: true,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
